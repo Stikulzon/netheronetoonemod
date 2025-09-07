99e4b1926c9f7e705799db4020ddb4971567049d
Zefir <stickulzon@gmail.com>
2025-09-07 21:23:25 +0200

    update to 1.21.6-8

--- src/main/java/com/netheronetoonemod/mixin/DimensionTypeMixin.java
+++ src/main/java/com/netheronetoonemod/mixin/DimensionTypeMixin.java
@@ -1,13 +1,12 @@
 package com.netheronetoonemod.mixin;
 
-import net.minecraft.world.dimension.DimensionType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(DimensionType.class)
-public class ExampleModMixin {
+@Mixin(net.minecraft.world.dimension.DimensionType.class)
+public class DimensionTypeMixin {
     @Inject(method = "coordinateScale", at = @At("HEAD"), cancellable = true)
     private void onCoordinateScale(CallbackInfoReturnable<Double> cir) {
         cir.setReturnValue(1.0);
